8a63e6c4d9cd0b49de0f84ad2d4d440ac8d6e285
hansjd - Hans Jákup Danielsen <hansjd@hotmail.com>
2015-02-24 16:47:24 +0100

    added rest files

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// MongoDB
+//mongoose.connect('mongoose://localhost/rest_test');
+
+// Express
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', require('./routes/api'));
+
+// Start server
+var port = 1337;
+app.listen(port);
+console.log('API is running on port ' + port);
